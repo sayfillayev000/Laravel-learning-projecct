53aedff56f40522656e34cff3f58552da91dd164
Muhammad <muhammadsayfillayev1027@gmail.com>
2024-03-19 14:15:57 +0500

    user

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,7 @@ class User extends Authenticatable
 
     protected function name(): Attribute
     {
+        //////
         return Attribute::make(
             get: fn (string $value) => ucfirst($value),
         );
